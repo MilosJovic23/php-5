945b7d4967fc90ba9b6f2f8287f9c37f1db20199
MilosJovic23 <milos.mj992@gmail.com>
2024-10-29 10:02:25 +0100

    * homework

---
+++ DomaciForma.php
@@ -0,0 +1,23 @@
+
+
+
+
+
+<!doctype html>
+<html lang="en">
+    <head>
+
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="DomaciPodaci.php" method="GET">
+            <input type="text" name="cena"><br>
+            <select name="vrstaRobe" >
+                <option>Hrana</option>
+                <option>Oprema za racunare</option>
+            </select>
+            <input type="checkbox" name="porez">
+            <button type="submit">Izracuna cenu</button>
+        </form>
+    </body>
+</html>
