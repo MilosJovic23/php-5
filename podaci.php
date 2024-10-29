4a019893b44f94c02262249047709fb30257fe09
MilosJovic23 <milos.mj992@gmail.com>
2024-10-29 10:04:03 +0100

    * Php-5

---
+++ podaci.php
@@ -0,0 +1,16 @@
+
+<?php
+
+    // dasd
+
+
+    if ( isset( $_GET["proveraSigurnosti"] ) ) {
+        echo $_GET["proveraSigurnosti"];
+    } else {
+        echo "Niste cekirali sigurnost";
+    }
+
+
+
+
+?>
