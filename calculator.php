3baa3ce4b5bc0ca2de8e2459e6835908ac2ee398
MilosJovic23 <milos.mj992@gmail.com>
2024-10-29 10:02:07 +0100

    * small changes

---
+++ calculator.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+
+    // $_SERVER , $_GET , $_POST  ->  assoc array   - global variables
+
+
+
+
+    $rezultatSabiranja = $_GET["Broj1"] + $_GET["Broj2"];
+    $rezultatOduzimanja = $_GET["Broj1"] - $_GET["Broj2"];
+
+    $vrstaRacunanja = $_GET["vrsta_racunanja"];
+
+
+    if( $vrstaRacunanja == "sabiranje" ){
+        echo $rezultatSabiranja;
+    } else {
+        echo $rezultatOduzimanja;
+    }
+
+
+?>
