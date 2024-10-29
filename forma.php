2a88443a02ba7e8c6b226c11967cf1345c73c83b
MilosJovic23 <milos.mj992@gmail.com>
2024-10-29 10:03:53 +0100

    * Php-5

---
+++ forma.php
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+<!doctype html>
+    <html lang="en">
+        <head>
+            <title>Document</title>
+        </head>
+        <body>
+
+            <form action="podaci.php" method="GET">
+                <input type="checkbox" name="proveraSigurnosti">
+                <button type="submit">test</button>
+            </form>
+        
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+
+
+<!doctype html>
+    <html lang="en">
+    <head>
+        <title>Document</title>
+    </head>
+    <body>
+
+    <form method="GET" action="calculator.php">
+        <input type="text" name="Broj1">
+        <input type="text" name="Broj2" >
+        <select name="vrsta_racunanja">
+            <option>sabiranje</option>
+            <option>oduzimanje</option>
+        </select>
+        <button type="submit">izracunaj</button>
+    </form>
+
+    </body>
+</html>
